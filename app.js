7d899d9d82cb06f3915c4e382bae2c8c6bfff1fc
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-05-12 23:21:02 +0300

    fixes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express()
+const cors = require('cors');
+
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger');
@@ -10,10 +13,16 @@ const user = require('./routes/user.routes');
 const userProduct = require('./routes/user.products.routes')
 const auth = require('./routes/auth.routes');
 
+app.use(cors({
+  // origin: '*'
+  origin: ['http://localhost:4200']
+}))
+
+
 app.use('/api/auth', auth);
 app.use('/api/users', user);
 app.use('/api/user-product', userProduct);
-app.use(express.static('files'));
+app.use('/', express.static('files'));
 
 app.use(
   '/api-docs', 
